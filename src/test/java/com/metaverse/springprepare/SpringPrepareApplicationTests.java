69e98613f5a0ac86ae86dbfed46df4e10f019ca4
Hynjun_Oh <dhguswns75@gmail.com>
2025-07-02 11:20:06 +0900

    chore: First init

---
+++ src/test/java/com/metaverse/springprepare/SpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.springprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
